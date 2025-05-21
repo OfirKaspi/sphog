391f768cd287f290b03f8cc4d01a6fa60ed78704
OfirKaspi <kaspiofir@gmail.com>
2025-05-21 11:47:47 +0300

    build fix, comment not relevant import

--- src/components/forms/leave-details-form/LeaveDetailsForm.tsx
+++ src/components/forms/leave-details-form/LeaveDetailsForm.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useState } from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectItem,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
