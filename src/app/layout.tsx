62b9a972b04ead0fda3cb596f17882bab38978fa
OfirKaspi <kaspiofir@gmail.com>
2025-06-21 15:26:18 +0300

    GSC - in order to remove the vercel subdomain v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,9 +38,6 @@ export const metadata = {
   icons: {
     icon: "/logo.png",
   },
-  other: {
-    "google-site-verification": "6uzj0VDyGSEYwFla9yPnbnE9PkCC1aj3xDWNUIY81VM"
-  },
 };
 
 export default function RootLayout({
@@ -48,6 +45,10 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="he" dir="rtl">
+      <head>
+        <meta name="google-site-verification" content="G7SewZLWCAWM1NrPBh8IgNvzzkJyLUFMT3QyHXGuVuw" />
+        <meta name="google-site-verification" content="6uzj0VDyGSEYwFla9yPnbnE9PkCC1aj3xDWNUIY81VM" />
+      </head>
       <body className="antialiased bg-background text-text font-fredoka">
         <Navbar />
         {children}
